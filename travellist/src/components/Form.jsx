420d9564723f7f28f8ecf1bc53a396193d3837a5
MatthewOfomi <ofomimatthew7@gmail.com>
2024-02-06 12:07:15 +0100

    flash card min task

--- travellist/src/components/Form.jsx
+++ travellist/src/components/Form.jsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit(e){
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
 
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What Items do you need for your vacation ?</h3>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) =>{setDescription(e.target.value)}}
+      />
       <button>Add Item</button>
     </form>
   );
